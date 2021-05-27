d2ad38cf5f3031e90cd0447750e817fd9c531222
Andrew Borondia <andrew>
2021-05-27 11:03:24 -0500

    add functionality to populate arrays with filtered search results

--- src/js/mapBoxApiHandler.js
+++ src/js/mapBoxApiHandler.js
@@ -1,7 +1,7 @@
 class MapBox {
   constructor() {
-    this.taraska = { lat: 49.94517344097148, lon: -97.19984167278717 };
-    this.luxton = { lat: 49.92505890981441, lon: -97.11720242553508 };
+    this.currentOriginResults = [];
+    this.currentDestinationResults = [];
     this.bboxWinnipeg = { minLon: -97.325875, minLat: 49.766204, maxLon: -96.953987, maxLat: 49.99275 };
     this.originFetchUrl = { get: () => `https://api.mapbox.com/geocoding/v5/mapbox.places/${UI.originInputValue.get()}.json?bbox=-97.325875,%2049.766204,%20-96.953987,%2049.99275&access_token=pk.eyJ1IjoiYWJvcm9uZGlhIiwiYSI6ImNrcDRxNDc1ODA0YTEybm5xcGl0bXU5N3AifQ.HKd0aKNsdN7FAtDTanMDWg&limit=10` };
     this.destinationFetchUrl = { get: () => `https://api.mapbox.com/geocoding/v5/mapbox.places/${UI.destinationInputValue.get()}.json?bbox=-97.325875,%2049.766204,%20-96.953987,%2049.99275&access_token=pk.eyJ1IjoiYWJvcm9uZGlhIiwiYSI6ImNrcDRxNDc1ODA0YTEybm5xcGl0bXU5N3AifQ.HKd0aKNsdN7FAtDTanMDWg&limit=10` };
@@ -14,15 +14,27 @@ class MapBox {
     return data;
   }
 
+  getFilteredSearchResults = (searchResults, arrayToPopulate) => {
+    searchResults.features.forEach(result => {
+      arrayToPopulate.push({
+        name: result.text,
+        address: result.properties.address,
+        lat: result.center[1],
+        lon: result.center[0],
+      })
+    })
+  }
+
   getOriginSearchResults = () => {
     this.getData(this.originFetchUrl.get())
-      .then(data => console.log(data))
+      .then(searchResults => this.getFilteredSearchResults(searchResults, this.currentOriginResults))
+      .then(() => console.log('Origin: ', this.currentOriginResults))
   }
 
   getDestinationSearchResults = () => {
     this.getData(this.destinationFetchUrl.get())
-      .then(data => console.log(data))
-  }
+    .then(searchResults => this.getFilteredSearchResults(searchResults, this.currentDestinationResults))
+    .then(() => console.log('Destination: ', this.currentDestinationResults))  }
 }
 
 const mapBox = new MapBox();

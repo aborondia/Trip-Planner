7613197061289f2f25b948237768ca7514f7aa8d
Andrew Borondia <andrew>
2021-05-27 09:41:44 -0500

    make UI class static

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,12 +1,10 @@
 class UI {
-  constructor() {
-    this.originFormEl = { get: () => document.getElementById('origin-form') };
-    this.destinationFormEl = { get: () => document.getElementById('destination-form') };
-    this.originInputValue = { get: () => this.originFormEl.get().firstElementChild.value };
-    this.destinationInputValue = { get: () => this.destinationFormEl.get().firstElementChild.value };
-  }
+    static originFormEl = { get: () => document.getElementById('origin-form') };
+    static destinationFormEl = { get: () => document.getElementById('destination-form') };
+    static originInputValue = { get: () => this.originFormEl.get().firstElementChild.value };
+    static destinationInputValue = { get: () => this.destinationFormEl.get().firstElementChild.value };
 
-  handleFormSubmit(target) {
+  static handleFormSubmit(target) {
     if (target === this.originFormEl.get()) {
       console.log(this.originInputValue.get());
       Renderer.renderPage();
@@ -20,11 +18,9 @@ class UI {
 
 }
 
-const ui = new UI();
-
 document.body.addEventListener('submit', (event) => {
   const target = event.target;
 
   event.preventDefault();
-  ui.handleFormSubmit(target);
+  UI.handleFormSubmit(target);
 })
